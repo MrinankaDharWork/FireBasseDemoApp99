5214dbe0d2ee45be278896f067e7d75daa0cc223
L037185 <dhar_mrinanka@lilly.com>
2022-09-26 01:55:41 +0530

    navigation to login page done

--- app/src/main/java/com/example/firebassedemoapp/ui/WelcomePage.kt
+++ app/src/main/java/com/example/firebassedemoapp/ui/WelcomePage.kt
@@ -7,20 +7,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.firebassedemoapp.R
 import com.example.firebassedemoapp.databinding.FragmentWelcomePageBinding
 
-//@SuppressLint("StaticFieldLeak")
-//private lateinit var binding : FragmentWelcomePageBinding
 
-class WelcomePage : Fragment(R.layout.fragment_welcome_page) {
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-////        binding.login.setOnClickListener {
-////            Navigation.findNavController(requireActivity(),R.id.action_welcomePage_to_loginPage)
-////        }
-//    }
+
+
+class WelcomePage : Fragment() {
+    private  var _binding : FragmentWelcomePageBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View{
+        // Inflate the layout for this fragment
+        _binding = FragmentWelcomePageBinding.inflate(inflater, container, false)
+
+        binding.login.setOnClickListener {
+            //Navigation.findNavController(view).navigate(R.id.action_welcomePage_to_loginPage)
+            //Navigation.findNavController(requireActivity(),R.id.welcomePage).navigate(R.id.action_welcomePage_to_loginPage)
+            //it.findNavController().navigate(R.id.action_welcomePage_to_loginPage)
+
+            it.findNavController().navigate(R.id.action_welcomePage_to_loginPage)
+        }
+
+        return binding.root
+    }
 
 }
