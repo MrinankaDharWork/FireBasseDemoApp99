8327884d6f38f6eea53ad19c0b8fe08313cf8465
L037185 <dhar_mrinanka@lilly.com>
2022-09-26 00:26:05 +0530

    Basic layouts assed

---
+++ app/src/main/java/com/example/firebassedemoapp/ui/WelcomePage.kt
@@ -0,0 +1,13 @@
+package com.example.firebassedemoapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.firebassedemoapp.R
+
+
+class WelcomePage : Fragment(R.layout.fragment_welcome_page) {
+
+}
