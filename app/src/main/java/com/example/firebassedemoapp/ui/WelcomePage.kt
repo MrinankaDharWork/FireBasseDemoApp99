d64c453c62613424ed073364d271f9aa22b86bc4
L037185 <dhar_mrinanka@lilly.com>
2022-09-26 01:21:13 +0530

    navigation to login page

--- app/src/main/java/com/example/firebassedemoapp/ui/WelcomePage.kt
+++ app/src/main/java/com/example/firebassedemoapp/ui/WelcomePage.kt
@@ -1,13 +1,26 @@
 package com.example.firebassedemoapp.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.firebassedemoapp.R
+import com.example.firebassedemoapp.databinding.FragmentWelcomePageBinding
 
+//@SuppressLint("StaticFieldLeak")
+//private lateinit var binding : FragmentWelcomePageBinding
 
 class WelcomePage : Fragment(R.layout.fragment_welcome_page) {
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+////        binding.login.setOnClickListener {
+////            Navigation.findNavController(requireActivity(),R.id.action_welcomePage_to_loginPage)
+////        }
+//    }
 
 }
